69ea6edb39ed5361654e192df8e4d3f8c5c175fd
Perry Vargas <perrybvargas@gmail.com>
2021-01-07 08:15:43 -0800

    Fixes #6: Create pyinstaller distribution

--- tasks.py
+++ tasks.py
@@ -16,14 +16,19 @@ def format(c):
 
 
 @task
+def bootstrap(c):
+    import comtypes.client
+    comtypes.client.GetModule(r"C:\\Windows\\System32\\UIAutomationCore.dll")
+
+@task(bootstrap)
 def package(c):
     windows = (Path(__file__) / ".." / "windows").resolve()
     if windows.exists():
         shutil.rmtree(windows)
     c.run("briefcase create")
     c.run("briefcase build")
-    # The first time it runs it fails but the next time it works. Im pretty sure it's because win32
-    # needs to bootstrap and it's easier to do that by failing the first run through
+    # The first time it runs it fails but the next time it works. This is to bootstrap the uia
+    # automation files for comtypes
     c.run("briefcase run", echo=False, warn=True)
     c.run("briefcase package")
 
@@ -33,3 +38,8 @@ def install(c):
     windows = (Path(__file__) / ".." / "windows").resolve()
     msi = list(windows.glob("*.msi"))[0]
     c.run(str(msi))
+
+
+@task(bootstrap)
+def exe(c):
+    c.run("pyinstaller winsnap.spec")
